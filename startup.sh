c89453f896fe2647a9ce48752c36518da36cf574
MajikJoker <33824823+MajikJoker@users.noreply.github.com>
2024-07-19 20:52:24 +0800

    debugging

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@
 # Activate the virtual environment
 source antenv/bin/activate
 # Start the Flask application
-exec gunicorn --bind 0.0.0.0:8000 IBAS:app
+exec gunicorn --bind 0.0.0.0:8000 --workers 3 --timeout 120 --graceful-timeout 120 --log-level debug --access-logfile '-' --error-logfile '-' IBAS:app
